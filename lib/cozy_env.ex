82a330af5b035c54dcb865df92e2ee6f629d34ac
c4710n <c4710n@users.noreply.github.com>
2024-01-07 18:37:26 +0800

    feat: add code that works

--- lib/cozy_env.ex
+++ lib/cozy_env.ex
@@ -1,18 +1,172 @@
 defmodule CozyEnv do
   @moduledoc """
-  Documentation for `CozyEnv`.
+  Helpers for handling OS environment variables.
   """
 
+  alias CozyEnv.EnvError
+
+  @type data_types ::
+          :boolean
+          | :integer
+          | :float
+          | :string
+          | :atom
+
+  @type result ::
+          boolean()
+          | integer()
+          | float()
+          | String.t()
+          | atom()
+
+  @type option :: {:message, String.t()}
+  @type options :: [option()]
+
+  @supported_types [
+    :boolean,
+    :integer,
+    :float,
+    :string,
+    :atom
+  ]
+
   @doc """
-  Hello world.
+  Gets the value of the given environment variable, and casts it into given type.
+
+  If the environment variable is not set, returns `nil`.
 
   ## Examples
 
-      iex> CozyEnv.hello()
-      :world
+     iex> CozyEnv.get_env("NOT_SET", :boolean)
+     nil
+
+     # export PHX_SERVER=true
+     iex> CozyEnv.get_env("PHX_SERVER", :boolean)
+     true
+
+     # export DB_POOL_SIZE=10
+     iex> CozyEnv.get_env("DB_POOL_SIZE", :integer)
+     10
+
+     # export PERCENT=0.95
+     iex> CozyEnv.get_env("PERCENT", :float)
+     0.95
+
+     # export USER=billy
+     iex> CozyEnv.get_env("USER", :string)
+     "billy"
+
+     # export USER=billy
+     iex> CozyEnv.get_env("USER", :atom)
+     :billy
 
   """
-  def hello do
-    :world
+  @spec get_env(String.t(), data_types()) :: result()
+  def get_env(varname, type)
+      when is_binary(varname) and type in @supported_types do
+    case System.fetch_env(varname) do
+      {:ok, value} ->
+        apply(__MODULE__, :"to_#{type}", [varname, value])
+
+      :error ->
+        nil
+    end
   end
+
+  @doc """
+  Gets the value of the given environment variable, and casts it into given type.
+
+  If the environment variable is not set, raises an error.
+
+  ## Examples
+
+     iex> CozyEnv.fetch_env!("NOT_SET", :boolean)
+     nil
+
+     # export PHX_SERVER=true
+     iex> CozyEnv.fetch_env!("PHX_SERVER", :boolean)
+     true
+
+     # export DB_POOL_SIZE=10
+     iex> CozyEnv.fetch_env!("DB_POOL_SIZE", :integer)
+     10
+
+     # export PERCENT=0.95
+     iex> CozyEnv.fetch_env!("PERCENT", :float)
+     0.95
+
+     # export USER=billy
+     iex> CozyEnv.fetch_env!("USER", :string)
+     "billy"
+
+     # export USER=billy
+     iex> CozyEnv.fetch_env!("USER", :atom)
+     :billy
+
+  """
+  @spec fetch_env!(String.t(), data_types(), options()) :: result()
+  def fetch_env!(varname, type, opts \\ [])
+      when is_binary(varname) and type in @supported_types do
+    case System.fetch_env(varname) do
+      {:ok, value} ->
+        apply(__MODULE__, :"to_#{type}", [varname, value])
+
+      :error ->
+        default_message = "environment variable #{varname} is missing"
+        extra_message = Keyword.get(opts, :message)
+
+        message =
+          if extra_message do
+            """
+            #{default_message}.
+            #{extra_message}
+            """
+          else
+            default_message
+          end
+
+        raise EnvError, message
+    end
+  end
+
+  @doc false
+  def to_boolean(_varname, "false"), do: false
+  def to_boolean(_varname, "0"), do: false
+  def to_boolean(_varname, ""), do: false
+  def to_boolean(_varname, _), do: true
+
+  @doc false
+  def to_integer(varname, value) do
+    try do
+      String.to_integer(value)
+    rescue
+      ArgumentError ->
+        raise EnvError,
+              "environment variable #{varname} is provided, but the value " <>
+                "#{inspect(value)} is not the string representation of an integer"
+    end
+  end
+
+  @doc false
+  def to_float(varname, value) do
+    case Float.parse(value) do
+      {float, ""} ->
+        float
+
+      _ ->
+        raise EnvError,
+              "environment variable #{varname} is provided, but the value " <>
+                "#{inspect(value)} is not the string representation of a float"
+    end
+  end
+
+  @doc false
+  def to_string(_varname, value), do: value
+
+  @doc false
+  def to_atom(_varname, value), do: String.to_atom(value)
+end
+
+defmodule CozyEnv.EnvError do
+  defexception [:message]
 end
--- test/cozy_env_test.exs
+++ test/cozy_env_test.exs
@@ -1,8 +1,145 @@
 defmodule CozyEnvTest do
   use ExUnit.Case
-  doctest CozyEnv
 
-  test "greets the world" do
-    assert CozyEnv.hello() == :world
+  setup context do
+    if env = context[:env] do
+      {varname, value} = env
+      System.put_env(varname, value)
+      on_exit(fn -> System.delete_env(varname) end)
+    end
+
+    :ok
+  end
+
+  describe "get_env/2" do
+    test "not set" do
+      assert nil == CozyEnv.get_env("VAR", :boolean)
+    end
+
+    @tag env: {"VAR", "true"}
+    test "type - boolean - true" do
+      assert true == CozyEnv.get_env("VAR", :boolean)
+    end
+
+    @tag env: {"VAR", "0"}
+    test "type - boolean - false" do
+      assert false == CozyEnv.get_env("VAR", :boolean)
+    end
+
+    @tag env: {"VAR", "10"}
+    test "type - integer" do
+      assert 10 == CozyEnv.get_env("VAR", :integer)
+    end
+
+    @tag env: {"VAR", "bad"}
+    test "type - integer - error" do
+      assert_raise CozyEnv.EnvError,
+                   "environment variable VAR is provided, but the value \"bad\" is not the string representation of an integer",
+                   fn ->
+                     CozyEnv.get_env("VAR", :integer)
+                   end
+    end
+
+    @tag env: {"VAR", "11"}
+    test "type - float without a decimal point" do
+      assert 11.0 == CozyEnv.get_env("VAR", :float)
+    end
+
+    @tag env: {"VAR", "11.0"}
+    test "type - float with a decimal point" do
+      assert 11.0 == CozyEnv.get_env("VAR", :float)
+    end
+
+    @tag env: {"VAR", "bad"}
+    test "type - float - error" do
+      assert_raise CozyEnv.EnvError,
+                   "environment variable VAR is provided, but the value \"bad\" is not the string representation of a float",
+                   fn ->
+                     CozyEnv.get_env("VAR", :float)
+                   end
+    end
+
+    @tag env: {"VAR", "hello"}
+    test "type - string" do
+      assert "hello" == CozyEnv.get_env("VAR", :string)
+    end
+
+    @tag env: {"VAR", "hello"}
+    test "type - atom" do
+      assert :hello == CozyEnv.get_env("VAR", :atom)
+    end
+  end
+
+  describe "fetch_env!/3" do
+    test "not set" do
+      assert_raise CozyEnv.EnvError,
+                   "environment variable VAR is missing",
+                   fn ->
+                     CozyEnv.fetch_env!("VAR", :boolean)
+                   end
+    end
+
+    test "not set - extra message" do
+      assert_raise CozyEnv.EnvError,
+                   "environment variable VAR is missing.\nSet it to something like: ecto://USER:PASS@HOST/DATABASE\n",
+                   fn ->
+                     CozyEnv.fetch_env!("VAR", :boolean,
+                       message: "Set it to something like: ecto://USER:PASS@HOST/DATABASE"
+                     )
+                   end
+    end
+
+    @tag env: {"VAR", "true"}
+    test "type - boolean - true" do
+      assert true == CozyEnv.fetch_env!("VAR", :boolean)
+    end
+
+    @tag env: {"VAR", "0"}
+    test "type - boolean - false" do
+      assert false == CozyEnv.fetch_env!("VAR", :boolean)
+    end
+
+    @tag env: {"VAR", "10"}
+    test "type - integer" do
+      assert 10 == CozyEnv.fetch_env!("VAR", :integer)
+    end
+
+    @tag env: {"VAR", "bad"}
+    test "type - integer - error" do
+      assert_raise CozyEnv.EnvError,
+                   "environment variable VAR is provided, but the value \"bad\" is not the string representation of an integer",
+                   fn ->
+                     CozyEnv.fetch_env!("VAR", :integer)
+                   end
+    end
+
+    @tag env: {"VAR", "11"}
+    test "type - float without a decimal point" do
+      assert 11.0 == CozyEnv.fetch_env!("VAR", :float)
+    end
+
+    @tag env: {"VAR", "11.0"}
+    test "type - float with a decimal point" do
+      assert 11.0 == CozyEnv.fetch_env!("VAR", :float)
+    end
+
+    @tag env: {"VAR", "bad"}
+    test "type - float - error" do
+      assert_raise CozyEnv.EnvError,
+                   "environment variable VAR is provided, but the value \"bad\" is not the string representation of a float",
+                   fn ->
+                     CozyEnv.fetch_env!("VAR", :float)
+                   end
+    end
+
+    @tag env: {"VAR", "hello"}
+    test "type - string" do
+      assert "hello" == CozyEnv.fetch_env!("VAR", :string)
+    end
+
+    @tag env: {"VAR", "hello"}
+    test "type - atom" do
+      assert :hello == CozyEnv.fetch_env!("VAR", :atom)
+    end
   end
 end
