d087508aca3b3b2caa119cdc254c302741bdf72b
c4710n <c4710n@users.noreply.github.com>
2024-01-08 08:50:34 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CozyEnv.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @description "Helpers for handling OS environment variables."
   @source_url "https://github.com/cozy-elixir/cozy_env"
 
