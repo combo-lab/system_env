397e669bc3c7b6dc7fa94d499fa5765b5e344b6d
Zeke Dou <zeke@zekedou.live>
2025-09-12 11:33:42 +0800

    release: bump v0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SystemEnv.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "Helpers for handling OS environment variables."
   @source_url "https://github.com/combo-lab/system_env"
 
